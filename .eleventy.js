4e4f3988cc8a16ea529a907d837aba11797a404f
BluMilkshake <75545969+BluMilkshake@users.noreply.github.com>
2022-11-21 23:32:31 +0200

    did some stuff, idk how to use git properly

--- .eleventy.js
+++ .eleventy.js
@@ -38,6 +38,9 @@ module.exports = function (eleventyConfig) {
   // Copy favicon to route of /_site
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
 
+  // The current year shortcode
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
   // Minify HTML
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
     // Eleventy 1.0+: use this.inputPath and this.outputPath instead
